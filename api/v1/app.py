11dcd2cf263254a43657f99726b4e44c904009ec
thomasomotayo <thomasomotayo@gmail.com>
2023-05-29 13:24:40 +0000

    by thomas

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,35 +1,27 @@
 #!/usr/bin/python3
-""" This module starts a flask application """
+"""app.py to connect to API"""
 import os
-from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify, make_response
 from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def remove(exception):
-    """ removes the current session """
-
+def teardown_appcontext(code):
+    """teardown_appcontext"""
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(e):
-    """ configures 404 error response """
-
-    return jsonify({"error": "Not found"}), e.code
-
+def page_not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
-    env_host = os.getenv("HBNB_API_HOST")
-    env_port = os.getenv("HBNB_API_PORT")
-
-    app.run(host=env_host if env_host else "0.0.0.0",
-            port=env_port if env_port else 5000,
-            threaded=True)
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
